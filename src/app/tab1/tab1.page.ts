b8eef210d968f335af37e38abc027e5c396acbc4
frufrutek <44049943+frufrutek@users.noreply.github.com>
2018-12-28 19:00:39 +0100

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -12,6 +12,7 @@ import { Storage } from "@ionic/storage";
 
 export class Tab1Page {
   public podatek:string;
+  public podatek_shrani:Array<Object>;
   constructor(private podatki: ZahteveService, private shramba: Storage) {}
 
   doRefresh(refresher) {
@@ -25,15 +26,18 @@ export class Tab1Page {
 }
 
   ngOnInit() {
-
     this.podatki.najnovejsi(1).then(data =>{
         this.podatek = JSON.parse(data.data).results;
       });
 }
 
-favorite(id,naslov){
-  console.log(id);
-  console.log(naslov);
+favorite(id){
+  this.podatek_shrani = [];
+  this.podatki.dobiPoId(id).then(data =>{
+      this.podatek_shrani = JSON.parse(data.data);
+    });
+  console.log("ID: " + id + " Podatki: " + this.podatek_shrani);
+  this.shramba.set(id, this.podatek_shrani);
 }
 
 }
